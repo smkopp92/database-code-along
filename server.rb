5ac37f5887232c9ec8fed6b95c2d5f4a1a565e94
Sebastian Kopp <smkopp92@gmail.com>
2017-11-16 14:58:10 -0500

    solution code

--- server.rb
+++ server.rb
@@ -1,6 +1,16 @@
 require "sinatra"
 require "sinatra/json"
 require "json"
+require "pg"
+
+def db_connection
+  begin
+    connection = PG.connect(dbname: "books")
+    yield(connection)
+  ensure
+    connection.close
+  end
+end
 
 CURRENT_FILE_PATH = File.dirname(__FILE__)
 
@@ -9,11 +19,18 @@ def parsed_books_json_data
   JSON.parse(data)
 end
 
+def books_from_database
+  books = db_connection { |conn| conn.exec("SELECT * FROM books") }
+  {
+    books: books.map{ |book| book }
+  }
+end
+
 before do
   headers({ "Access-Control-Allow-Origin" => "*" })
 end
 
 get "/books.json" do
   status 200
-  json parsed_books_json_data
+  json books_from_database
 end
